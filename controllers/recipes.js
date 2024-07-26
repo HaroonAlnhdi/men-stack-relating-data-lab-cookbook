84be488280d1d09a72564f2f96fedae9819ed9a2
Haroon <you@example.com>
2024-07-26 17:43:15 +0300

    bulid create recipes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,10 +3,32 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js');
-const Recipe = require('../models/recipe.js');
+const recipe = require('../models/recipe.js');
 
-router.get('/', async (req, res) => {
-    res.render('recipes/index.ejs');
-  });
+router.get('/index', async (req, res) => {
+  res.render('recipes/index.ejs');
+});
+
+router.get('/new', async (req, res) => {
+  res.render('recipes/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const user = req.session.user;
+    // Create a new Recipe object using req.body
+    const newRecipe = new recipe(req.body);
+    console.log(req.body);
+    newRecipe.owner = user._id
+    // Save the new Recipe
+    await newRecipe.save();
+
+    // Redirect back to the recipe index view
+    res.redirect('recipes/index.ejs');
+  } catch (error) {
+    console.error('Error creating recipe:', error);
+    res.redirect('/');
+  }
+});
 
 module.exports = router;
